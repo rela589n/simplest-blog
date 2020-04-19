6f668a26d9f5efdacd4cdbb5eb6bb2d425a85315
rela589n <zsidelnik@gmail.com>
2020-04-19 20:34:14 +0300

    Fix. Wrong seeding of posts images.

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Lib\Faker\Providers\PicsumImage;
 use App\Models\Post;
 use Illuminate\Database\Seeder;
 
@@ -15,14 +16,16 @@ class PostSeeder extends Seeder
     public function run()
     {
         $faker = Faker\Factory::create();
+        $faker->addProvider(new PicsumImage($faker));
 
         for ($i = 0; $i < self::POSTS_LIMIT; ++$i) {
+            $image = $faker->image('public/storage/uploads', 450, 180, null, false);
 
             Post::create([
-                'name' => $faker->realText(20),
-                'content' => $faker->realText(1000),
-                'uri_alias' => $faker->unique()->slug(2),
-                'image_path' => $faker->unique()->imageUrl(),
+                'name'        => $faker->realText(20),
+                'content'     => $faker->realText(1000),
+                'uri_alias'   => $faker->unique()->slug(2),
+                'image_path'  => "uploads/$image",
                 'category_id' => $faker->numberBetween(1, CategorySeeder::CATEGORIES_LIMIT)
             ]);
         }
