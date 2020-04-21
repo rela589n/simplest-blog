94cebb11c21f711c22f9c6228712d4156bb7262c
rela589n <zsidelnik@gmail.com>
2020-04-21 19:48:42 +0300

    Refactoring. Main slider initialization moved into single script.

--- resources/views/pages/main/posts/index.blade.php
+++ resources/views/pages/main/posts/index.blade.php
@@ -31,29 +31,5 @@
 @section('bottom_scripts')
     @parent
     <script src="{{ asset('vendor/nivo/jquery.nivo.slider.js') }}" type="text/javascript"></script>
-
-    <script type="text/javascript">
-        $(window).load(function () {
-            $('#slider').nivoSlider({
-                effect: 'random',
-                slices: 15,
-                animSpeed: 500,
-                pauseTime: 3000,
-                startSlide: 0, //Set starting Slide (0 index)
-                directionNav: false,
-                directionNavHide: false, //Only show on hover
-                controlNav: false, //1,2,3...
-                controlNavThumbs: false, //Use thumbnails for Control Nav
-                pauseOnHover: true, //Stop animation while hovering
-                manualAdvance: false, //Force manual transitions
-                captionOpacity: 0.8, //Universal caption opacity
-                beforeChange: function () {
-                },
-                afterChange: function () {
-                },
-                slideshowEnd: function () {
-                } //Triggers after all slides have been shown
-            });
-        });
-    </script>
+    <script src="{{ asset('js/main-slider.js') }}" type="text/javascript"></script>
 @endsection
