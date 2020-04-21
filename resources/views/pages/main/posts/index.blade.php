30015c10721f4797f185326f11c044e339ef53b8
rela589n <zsidelnik@gmail.com>
2020-04-21 19:59:31 +0300

    Refactoring. Create separate component for slider.

--- resources/views/pages/main/posts/index.blade.php
+++ resources/views/pages/main/posts/index.blade.php
@@ -1,27 +1,9 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <div id="homepage_slider">
-        <div id="slider">
-            <a href="#"><img src="{{ asset('img/slideshow/01.jpg')  }}" alt="Slide 01"
-                             title="Nam fermentum lacus suscipit diam feugiat fringilla."/></a>
-            <a href="#"><img src="{{ asset('img/slideshow/02.jpg')  }}" alt="Slide 02"
-                             title="Proin bibendum est id velit tincidunt ut sodales ligula facilisis."/></a>
-            <a href="#"><img src="{{ asset('img/slideshow/03.jpg')  }}" alt="Slide 03"
-                             title="Fusce tincidunt diam eu metus iaculis hendrerit."/></a>
-            <a href="#"><img src="{{ asset('img/slideshow/04.jpg')  }}" alt="Slide 04"
-                             title="Nulla faucibus luctus quam eget placerat. "/></a>
-            <a href="#"><img src="{{ asset('img/slideshow/05.jpg')  }}" alt="Slide 05"
-                             title="Aliquam quis velit et sem vestibulum dignissim."/></a>
-        </div>
-    </div>
+    <x-main.slider/>
 
     <x-posts.catalogue :posts="$posts"/>
     {{ $posts->links() }}
 @endsection
 
-@section('bottom_scripts')
-    @parent
-    <script src="{{ asset('vendor/nivo/jquery.nivo.slider.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('js/main-slider.js') }}" type="text/javascript"></script>
-@endsection
