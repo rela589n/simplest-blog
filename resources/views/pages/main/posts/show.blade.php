91cb267d84a994a44c1b34171a9b6b8a23119a61
rela589n <zsidelnik@gmail.com>
2020-04-21 21:49:53 +0300

    Enhancement. Remove redundant content wrapper.

--- resources/views/pages/main/posts/show.blade.php
+++ resources/views/pages/main/posts/show.blade.php
@@ -1,24 +1,20 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <div id="tooplate_content">
-        <x-posts.representations.full :post="$post"/>
+    <x-posts.representations.full :post="$post"/>
 
-        <h3>Comments</h3>
-        <div id="comment_section">
-            <x-comments.catalogue :comments="$post->comments"/>
-        </div>
+    <h3>Comments</h3>
+    <div id="comment_section">
+        <x-comments.catalogue :comments="$post->comments"/>
+    </div>
 
-        <div id="comment_form">
-            <h3>Leave your comment</h3>
+    <div id="comment_form">
+        <h3>Leave your comment</h3>
 
-            <x-comments.form :send-action="route('api.comments.store')"
-                             :commentable-id="$post->id"
-                             commentable-type="post"
-                             send-method="POST"/>
-        </div>
-
-        <div class="cleaner"></div>
+        <x-comments.form :send-action="route('api.comments.store')"
+                         :commentable-id="$post->id"
+                         commentable-type="post"
+                         send-method="POST"/>
     </div>
 @endsection
 
